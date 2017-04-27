e786660613195fcc1f3867421957d1f175c97f4b
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2017-04-27 20:59:08 +0000

    reporte para imprimir

--- __openerp__.py
+++ __openerp__.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
 
 #
-# Este es el modulo de Topke
+# Este es el modulo de liquidaciones
 #
-# Status 1.0 - tested on Open ERP 6.1.1
+# Status 1.0 - tested on Odoo 9.0
 #
 
 {
@@ -14,11 +14,12 @@
     'author': 'Rodrigo Fernandez',
     'website': 'http://solucionesprisma.com/',
     'depends' : [ 'l10n_gt_extra' ],
-    'demo' : [ ],
     'data' : [
+        'views/report.xml',
         'views/bolson_view.xml',
         'views/invoice_view.xml',
         'views/payment_view.xml',
+        'views/reporte_bolson.xml',
     ],
     'installable': True,
     'certificate': '',
