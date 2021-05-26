7a83a9f38f9b89a14551c2606c8395ca484feb3e
rocketgithub <rodrigo@solucionesprisma.com>
2021-05-26 12:34:25 -0600

    Usar el id, no el objeto

--- models/bolson.py
+++ models/bolson.py
@@ -72,7 +72,7 @@ class BolsonBolson(models.Model):
                     'debit': -1 * total if total < 0 else 0,
                     'credit': total if total > 0 else 0,
                     #'account_id': rec.facturas[0].account_id.id,
-                    'account_id': rec.cuenta_desajuste,
+                    'account_id': rec.cuenta_desajuste.id,
                     'date_maturity': rec.fecha,
                 }))
 
