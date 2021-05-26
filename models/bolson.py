bffd431bb4fb4b96aa3af7f40e517dc00d59d6a1
rocketgithub <rodrigo@solucionesprisma.com>
2021-05-26 11:55:38 -0600

    Usar cuenta de desajuste

--- models/bolson.py
+++ models/bolson.py
@@ -71,7 +71,8 @@ class BolsonBolson(models.Model):
                     'name': 'Diferencial en ' + rec.name,
                     'debit': -1 * total if total < 0 else 0,
                     'credit': total if total > 0 else 0,
-                    'account_id': rec.facturas[0].account_id.id,
+                    #'account_id': rec.facturas[0].account_id.id,
+                    'account_id': rec.cuenta_desajuste,
                     'date_maturity': rec.fecha,
                 }))
 
